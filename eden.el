eb254aa928bf3cf05393b25300247b1edf0840cc
Tony Aldon <tony@tonyaldon.com>
2026-05-12 16:09:25 +0200

    release version 1.31

--- eden.el
+++ eden.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2025 Tony Aldon
 ;;
 ;; Author: Tony Aldon <tony@tonyaldon.com>
-;; Version: 1.30
+;; Version: 1.31
 ;; Package-Requires: ((emacs "28.1"))
 ;; Homepage: https://tonyaldon.com
 ;;
